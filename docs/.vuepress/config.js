011bd79a34edf4a7e43691c6d4ce322efd497da7
duanhairui <56431806+duanhairui@users.noreply.github.com>
2022-01-30 14:41:43 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
         [ 'meta', {name:'keywords',content:'IRITA,irita,IRITAOPB,iritaopb,IRITAHUB,iritahub,上海边界,边界智能区块链,边界智能区块链底层技术,分布式商业,企业联盟链,许可链,开放许可链,区块链底层技术框架,iService,IBC,区块链跨链,文昌链,BSN开放联盟链'}],
         [ 'meta', {name:'description',content:'IRITA 是边界智能以区块链跨链、隐私计算及大数据分析技术创新为核心，自主研发的支持下一代分布式商业系统的企业级联盟链产品线。IRITA 面向服务，针对开发者提供丰富的链应用开发服务套件，能更好的满足中小企业以更具成本效益的方式快速开发和部署 DApp 及其他链上应用的需求。'}],
         [ 'meta', {name: 'baidu-site-verification', content: 'code-jY9z5Dzz79' }],
-        [ 'meta', {name: '360-site-verification', content: 'f3b10913153efa7f64f5e3d15b2f816d' }],
+        [ 'meta', {name: '360-site-verification', content: '7f2a1617665f5d995f3cf90b824f29a5' }],
         [ 'meta', {property:'og:image',content:'https://www.bianjie.ai/resources/IRITA/Bianjie-Logo-IRITA-Vertical-Black.png'}],
         [ 'meta', {property:'og:title',content:'IRITA - 企业级联盟链产品线'}],
         [ 'meta', {property:'og:description',content:'IRITA 是边界智能以区块链跨链、隐私计算及大数据分析技术创新为核心，自主研发的支持下一代分布式商业系统的企业级联盟链产品线。IRITA 面向服务，针对开发者提供丰富的链应用开发服务套件，能更好的满足中小企业以更具成本效益的方式快速开发和部署 DApp 及其他链上应用的需求。'}],
