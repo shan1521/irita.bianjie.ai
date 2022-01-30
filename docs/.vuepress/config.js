a6d22a37285942e9c68228ec99785d3106ee39a4
shanshan <shan@bianjie.ai>
2022-01-30 15:29:10 +0800

    change icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'IRITA 是边界智能以区块链跨链、隐私计算及大数据分析技术创新为核心，自主研发的支持下一代分布式商业系统的企业级联盟链产品线。IRITA 面向服务，针对开发者提供丰富的链应用开发服务套件，能更好的满足中小企业以更具成本效益的方式快速开发和部署 DApp 及其他链上应用的需求。',
     base:'/',
     head: [
-        [ 'link', {rel:'icon',href:'/iritalogo.png'}],
+        [ 'link', {rel:'icon',href:'/favicon.ico'}],
         [ 'meta', {name:'keywords',content:'IRITA,irita,IRITAOPB,iritaopb,IRITAHUB,iritahub,上海边界,边界智能区块链,边界智能区块链底层技术,分布式商业,企业联盟链,许可链,开放许可链,区块链底层技术框架,iService,IBC,区块链跨链,文昌链,BSN开放联盟链'}],
         [ 'meta', {name:'description',content:'IRITA 是边界智能以区块链跨链、隐私计算及大数据分析技术创新为核心，自主研发的支持下一代分布式商业系统的企业级联盟链产品线。IRITA 面向服务，针对开发者提供丰富的链应用开发服务套件，能更好的满足中小企业以更具成本效益的方式快速开发和部署 DApp 及其他链上应用的需求。'}],
         [ 'meta', {name: 'baidu-site-verification', content: 'jY9z5Dzz79' }],
